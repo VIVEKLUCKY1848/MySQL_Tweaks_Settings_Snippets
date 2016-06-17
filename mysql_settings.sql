749b212659a76ceb508b3d757e66b3d0e0bb1840
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-06-17 22:02:27 +0530

    Update mysql_settings.sql

--- mysql_settings.sql
+++ mysql_settings.sql
@@ -1,1 +1,7 @@
-
+-- Turn on logging and store logs into table instead of file(default)
+SET GLOBAL log_output = 'TABLE';
+SET GLOBAL general_log = 'ON';
+-- Now open phpMyAdmin and select database "mysql" and go to table "general_log" to find your logged queries
+-- Also don't forget to revert these settings or your mysql server might become slow due to clogging of queries into this table.
+SET GLOBAL log_output = 'FILE';
+SET GLOBAL general_log = 'OFF';
